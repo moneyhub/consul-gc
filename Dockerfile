f6c455064ade9aee193bebe1dfe2316b3d915e2c
James Tancock <james.tancock@momentumft.co.uk>
2017-01-19 10:32:41 +0000

    Initial commit of consul-gc

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM alpine
+MAINTAINER James Tancock <james.tancock@momentumft.co.uk
+
+ENV GOPATH /go
+ENV REPOSRC /go/src/github.com/momentumft/consul-gc
+
+RUN apk update && apk add ca-certificates \
+    && rm -rf /var/cache/apk/*
+
+COPY . $REPOSRC
+
+RUN packages='go git gcc libc-dev libgcc' \
+    && apk update \
+    && apk add $packages \
+    && cd $REPOSRC \
+    && go get -v ./... \
+    && go build -x -o /usr/bin/consul-gc . \
+    && apk del $packages \
+    && rm -rf $GOPATH \
+    && rm -rf /var/cache/apl/*
+
+ENTRYPOINT ["/usr/bin/consul-gc"]
---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/crewjam/awsregion"
+	"github.com/crewjam/ec2cluster"
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/serf/serf"
+)
+
+const VERSION = "1.0.0"
+
+var (
+	v bool
+)
+
+func init() {
+	flag.BoolVar(&v, "v", false, "show version")
+	flag.Parse()
+}
+
+func getDesiredInstanceCount(instanceId string) (*int64, error) {
+	awsSession := session.New()
+	if region := os.Getenv("AWS_REGION"); region != "" {
+		awsSession.Config.WithRegion(region)
+	}
+	awsregion.GuessRegion(awsSession.Config)
+	s := &ec2cluster.Cluster{
+		AwsSession: awsSession,
+		InstanceID: instanceId,
+	}
+
+	asg, err := s.AutoscalingGroup()
+	// Not nice having two nil checks, but previous call has one scenario
+	// where both vars can be nil
+	if err != nil {
+		return nil, err
+	}
+
+	if asg != nil {
+		return asg.DesiredCapacity, nil
+	}
+
+	return nil, errors.New("this instance doesn't belong to an Auto Scaling Group")
+}
+
+func removeFailed(agent *api.Agent, failed []string) error {
+	for _, memberName := range failed {
+		err := agent.ForceLeave(memberName)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	if v {
+		fmt.Println(VERSION)
+		return
+	}
+
+	log.Printf("[INFO] Starting consul-gc service")
+
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		log.Printf("[FATAL] Failed to create consul api client. Exiting...")
+		os.Exit(2)
+	}
+
+	// Find the desired number of alive consul servers from the desired instances in the auto scaling group
+	instanceId, err := ec2cluster.DiscoverInstanceID()
+	if err != nil {
+		log.Printf("[FATAL] Could not find the ec2 instance ID: %s", err)
+	}
+
+	desiredActive, err := getDesiredInstanceCount(instanceId)
+	if err != nil {
+		log.Printf("[FATAL] Failed to find the desired number of servers: %s", err)
+		os.Exit(3)
+	}
+
+	agent := client.Agent()
+	for {
+		time.Sleep(time.Second * 60)
+
+		members, err := agent.Members(false)
+		if err != nil {
+			log.Printf("[ERROR] Failed to retrieve consul members: %s", err)
+			continue
+		}
+
+		alive := 0
+		var failed []string
+		failed = make([]string, len(members))
+		for i, member := range members {
+			if member.Status == int(serf.StatusAlive) {
+				alive++
+			}
+
+			if member.Status == int(serf.StatusFailed) {
+				failed[i] = member.Name
+			}
+		}
+
+		if alive >= int(*desiredActive) {
+			err := removeFailed(agent, failed)
+			if err != nil {
+				log.Printf("[ERROR] Failed to remove member: %s", err)
+			}
+		}
+	}
+}
